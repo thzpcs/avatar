5c344f832c70ed021f8a48a5cb8e3cdd4d9e2d25
thzpcs <37026570+thzpcs@users.noreply.github.com>
2018-05-31 10:44:44 -0600

    Delete kinectTest.py

--- kinectTest.py
+++
@@ -1,9 +0,0 @@
-import vpython
-
-import vpykinect
- 
-vpykinect.draw_sensor(frame())
-skeleton = vpykinect.Skeleton(frame(visible=False))
-while True:
-    rate(30)
-    skeleton.frame.visible = skeleton.update()
